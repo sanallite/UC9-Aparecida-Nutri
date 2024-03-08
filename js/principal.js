3c9a2c24fe0f9a1554c1cd8afb754333fcccfaa2
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-03-08 11:32:30 -0300

    Transformando o cálculo em um loop for.

--- js/principal.js
+++ js/principal.js
@@ -21,21 +21,27 @@ console.log(titulo_classe.textContent);
 let paciente = document.querySelector("#primeiro-paciente");
 // Selecionando o elemento com id.
 
-let td_peso = paciente.querySelector(".info-peso");
-let td_altura = paciente.querySelector(".info-altura");
-let td_gordura = paciente.querySelector(".info-gordura");
-let td_imc = paciente.querySelector(".info-imc");
-// Selecionando os elementos filhos através de suas classes, utilizando o nome da variável.
+let pacientes = document.querySelectorAll(".paciente");
+// console.log(pacientes);
+for (let i = 0; i < pacientes.length; i++) {
+	const paciente = pacientes[i];
+	
+	let td_peso = paciente.querySelector(".info-peso");
+	let td_altura = paciente.querySelector(".info-altura");
+	let td_gordura = paciente.querySelector(".info-gordura");
+	let td_imc = paciente.querySelector(".info-imc");
+	// Selecionando os elementos filhos através de suas classes, utilizando o nome da variável.
 
-let altura = td_altura.textContent;
-let peso = td_peso.textContent;
-// Definindo que os valores são o conteúdo daqueles elementos armazenados.
+	let altura = td_altura.textContent;
+	let peso = td_peso.textContent;
+	// Definindo que os valores são o conteúdo daqueles elementos armazenados.
 
-// let altura_quadrado = altura*altura;
-// let imc = peso/altura_quadrado;
-// let imc = peso / ( altura*altura );
-let imc = peso / Math.pow(altura, 2);
-// Fazendo o cálculo.
+	// let altura_quadrado = altura*altura;
+	// let imc = peso/altura_quadrado;
+	// let imc = peso / ( altura*altura );
+	let imc = peso / Math.pow(altura, 2);
+	// Fazendo o cálculo.
 
-td_imc.textContent = imc.toFixed(2);
-// Definindo o valor para aquele elemento, e adicionando duas casas decimais para exibição.
+	td_imc.textContent = imc.toFixed(2);
+	// Definindo o valor para aquele elemento, e adicionando duas casas decimais para exibição.
+}
