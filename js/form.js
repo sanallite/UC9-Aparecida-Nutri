9e5005bb4fd0e6726833006100a51854ac9ee76e
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-03-12 11:48:31 -0300

    Convertando para orientação a objetos/funções.

--- js/form.js
+++ js/form.js
@@ -4,32 +4,48 @@ botaoAdicionar.addEventListener("click", (event) => {
 	event.preventDefault();
 	// Prevenindo o comportamento padrão que acontece ao apertar o botão submit.
 
-	var form = document.querySelector("#adiciona");
-	var nome = form.nome.value;
-	var peso = form.peso.value;
-	var altura = form.altura.value;
-	var gordura = form.gordura.value;
-	// Colocando os valores na variável, que podem ser pegos pelo id ou name do elemento.
+    var form = document.querySelector("#adiciona");
+	var paciente = obterPaciente(form);
+	// Modo anterior: Colocando os valores na variável, que podem ser pegos pelo id ou name do elemento.
+    // Modo atual: Usando um objeto para pegar os dados do formulário.
+    console.log(paciente);
 
-	var paciente_tr = document.createElement("tr");
+	var paciente_tr = monta_tr(paciente);
+    // Criando o o elemento tr usando uma função.
 
-	var nome_td = document.createElement("td");
-	var peso_td = document.createElement("td");
-	var altura_td = document.createElement("td");
-	var gordura_td = document.createElement("td");
-	var imc_td = document.createElement("td");
-	// Criando os novos elementos.
+	var tabela_pacientes = document.querySelector("#tabela-pacientes");
+	tabela_pacientes.appendChild(paciente_tr);
+	// Selecionando um elemento tbody pelo seu id e acresentando o elemento tr como filho e por consquência os td como netos.
 
-	// var imc = peso / Math.pow(altura, 2);
-	// Fazendo o cálculo do IMC.
+	console.log("Nome:", nome,"\nPeso:", peso+"kg", "\nAltura:", altura+"m", "\nPorcentagem de gordura corporal:", gordura+"%");
+	// Modo anterior: Fazendo a exibição no console ficar do jeito que quero.
+    form.reset(); 
+});
 
-	nome_td.textContent = nome;
-	peso_td.textContent = peso;
-	altura_td.textContent = altura;
-	gordura_td.textContent = gordura;
-	imc_td.textContent = calculaIMC(peso, altura);
-	// Definindo o conteúdo dos novos elementos, com o imc tendo apenas duas casas decimais.
+// Nesse caso, como foi mais de um comando para executar, foi necessário colocar as chaves.
 
+// Objeto o paciente através da orientação a objetos.
+function obterPaciente(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaIMC(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function monta_tr(paciente) {
+    var paciente_tr = document.createElement("tr");
+    
+    var nome_td = monta_td(paciente.nome);
+	var peso_td = monta_td(paciente.peso);
+	var altura_td = monta_td(paciente.altura);
+	var gordura_td = monta_td(paciente.gordura);
+	var imc_td = monta_td(paciente.imc);
+	
 	paciente_tr.appendChild(nome_td);
 	paciente_tr.appendChild(peso_td);
 	paciente_tr.appendChild(altura_td);
@@ -37,12 +53,13 @@ botaoAdicionar.addEventListener("click", (event) => {
 	paciente_tr.appendChild(imc_td);
 	// Acresentando os elementos filhos td para o elemento tr criado.
 
-	var tabela_pacientes = document.querySelector("#tabela-pacientes");
-	tabela_pacientes.appendChild(paciente_tr);
-	// Selecionando um elemento tbody pelo seu id e acresentando o elemento tr como filho e por consquência os td como netos.
+    return paciente;
+}
 
-	console.log("Nome:", nome,"\nPeso:", peso+"kg", "\nAltura:", altura+"m", "\nPorcentagem de gordura corporal:", gordura+"%");
-	// Fazendo a exibição no console ficar do jeito que quero.
-});
+function monta_td(dados) {
+    var td = document.createElement("td");
+    td.textContent = dados;
 
-// Nesse caso, como foi mais de um comando para executar, foi necessário colocar as chaves.
+    return dados;
+}
+
